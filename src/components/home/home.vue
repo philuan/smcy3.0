1dd72b58175bd658088c40b1c06147276c2dd5ee
philuan <526593121@qq.com>
2018-10-24 18:30:32 +0800

    '配置文件'

---
+++ src/components/home/home.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="home">启动项目</div>
+</template>
+
+<script>
+export default {
+  name: 'home',
+  mounted () {
+    console.log(this.$store.state.show)
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
